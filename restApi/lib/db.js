07016891f5eb3ce4ecc3ebc8e06a588108c75057
Yaniv Keinan <yanivkeinan@gmail.com>
2016-02-09 15:58:56 +0200

    profile db actions

--- restApi/lib/db.js
+++ restApi/lib/db.js
@@ -21,76 +21,84 @@ var db = {
 module.exports = db;
 
 function put(params, cb, respondSuffix) {
-  //util.log.profile('db put');
+  var nStartTime = Date.now();
   return dynamoDoc.put(params, function(err, data) {
     if (respondSuffix && _.isEmpty(data)) data = respondSuffix;
+    var nEndTime = Date.now();
+    util.log.info('DB PUT Elapsed time: ' + String(nEndTime - nStartTime) + ' milliseconds');
     util.log.info(err, data);
-    //util.log.profile('db put');
     return cb(err, data);
   });
 }
 
 function get(params, cb) {
-  //util.log.profile('db get');
+  var nStartTime = Date.now();
   return dynamoDoc.get(params, function(err, data) {
     util.log.info(err, data);
     if (err) return {}; //err;
     if (_.isEmpty(data)) return cb(notFoundMsg);
-    //util.log.profile('db get');
+    var nEndTime = Date.now();
+    util.log.info('DB GET Elapsed time: ' + String(nEndTime - nStartTime) + ' milliseconds');
     return cb(err, data.Item);
   });
 }
 
 function query(params, cb) {
-  //util.log.profile('db query');
+  var nStartTime = Date.now();
   return dynamoDoc.query(params, function(err, data) {
     if (_.isEmpty(data)) return cb(notFoundMsg);
-    //util.log.profile('db query');
+    var nEndTime = Date.now();
+    util.log.info('DB Query Elapsed time: ' + String(nEndTime - nStartTime) + ' milliseconds');
     return cb(err, data.Items);
   });
 }
 
 function scan(params, cb) {
-  //util.log.profile('db scan');
+  var nStartTime = Date.now();
   return dynamoDoc.scan(params, function(err, data) {
     if (_.isEmpty(data)) return cb(notFoundMsg);
-    //util.log.profile('db scan');
+    var nEndTime = Date.now();
+    util.log.info('DB Scan Elapsed time: ' + String(nEndTime - nStartTime) + ' milliseconds');
     return cb(err, data.Items);
   });
 }
 
 function update(params, cb) {
-  //util.log.profile('db update');
+  var nStartTime = Date.now();
   return dynamoDoc.update(params, function(err, data) {
     util.log.info(err, data);
     if (_.isEmpty(data)) return cb(notFoundMsg);
-    //util.log.profile('db update');
+    var nEndTime = Date.now();
+    util.log.info('DB UPDATE Elapsed time: ' + String(nEndTime - nStartTime) + ' milliseconds');
     return cb(err, data.Attributes);
   });
 }
 
 function del(params, cb) {
-  //util.log.profile('db del');
+  var nStartTime = Date.now();
   return dynamoDoc.delete(params, function(err, data) {
     util.log.info(err, data);
     if (_.isEmpty(data)) return cb(notFoundMsg);
-    //util.log.profile('db del');
+    var nEndTime = Date.now();
+    util.log.info('DB DEL Elapsed time: ' + String(nEndTime - nStartTime) + ' milliseconds');
     return cb(err, data.Attributes);
   });
 }
 
 function batchGet(params, cb, table) {
-  //util.log.profile('db batchGet');
+  var nStartTime = Date.now();
   return dynamoDoc.batchGet(params, function(err, data) {
-    //util.log.profile('db batchGet');
+    var nEndTime = Date.now();
+    util.log.info('DB batchGet Elapsed time: ' + String(nEndTime - nStartTime) + ' milliseconds');
     return cb(err, data.Responses[table]);
   });
 }
 
 function batchWrite(params, cb, responseValue) {
-  //util.log.profile('db batchWrite');
+  var nStartTime = Date.now();
   return dynamoDoc.batchWrite(params, function(err, data) {
-    //util.log.profile('db batchWrite');
+    var nEndTime = Date.now();
+    util.log.info('DB batchWrite Elapsed time: ' + String(nEndTime - nStartTime) + ' milliseconds');
     return cb(err, responseValue);
   });
 }
