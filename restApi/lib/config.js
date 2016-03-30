0872a2bf950ca6b7a38c8e41c6e087f9d0e05412
Jelle Gerbrandy <github@gerbrandy.com>
2016-03-30 18:33:32 +0200

    comments in config.js

--- restApi/lib/config.js
+++ restApi/lib/config.js
@@ -2,17 +2,19 @@
 
 var config = {
   	tables: getTables(),
-	USER_INITIAL_TOKENS: 100, 
-	USER_INITIAL_REPUTATION: 0.2, 
+	USER_INITIAL_TOKENS: 100,  // number of tokens a user gets on creation
+	// TODO: this should be 0 (but it will break some tests)
+	USER_INITIAL_REPUTATION: 0.2, // initial reputation in percentage 
+	CONTRIBUTION_FEE: 1, // fee of contribution in tokens
+	REWARD_THRESHOLD: 0.3, // the treshold in percentage of upvotes after which users get rewarded
+	// if a user gets 100% upvotes, he should be rewarded USER_INITIAL_TOKENS
+	TOKEN_REWARD_FACTOR: 50, 
 	STAKE: 0.02, 
 	ALPHA: 0.7, 
 	GAMMA: 0.5, 
-	TOKEN_REWARD_FACTOR: 15,
 	REP_REWARD_FACTOR: 5,
-	CONTRIBUTION_FEE: 1,
 	DURATION: 86400000,
-	DISTRIBUTION_STAKE: 0.08,
-	REWARD_THRESHOLD: 0.3
+	DISTRIBUTION_STAKE: 0.08
 };
 
 module.exports = config;
